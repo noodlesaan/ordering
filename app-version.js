e0d5e5bb0e50b6100da7f0aff548c636da18eae4
sepandhaghighi <sepand.haghighi@yahoo.com>
2026-08-23 01:09:04 +0330

    rel : migrate to v1.0.1

--- app-version.js
+++ app-version.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0.0";
+const APP_VERSION = "1.0.1";
 
 document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll("[data-app-version]").forEach((element) => {
