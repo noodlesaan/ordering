557db907b7b201dafc76c0e8011c4d0372379ac6
Sepand Haghighi <sepand.haghighi@yahoo.com>
2026-08-23 13:37:43 +0330

    Update Data (#1)
    * fix : update CACHE_NAME
    
    * fix : update toppings
    
    * fix : rename ramens
    
    * fix : update others
    
    * rel : migrate to v1.0.1

--- pastOrders.js
+++ pastOrders.js
@@ -68,7 +68,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   const fingilishMap = {
     "رامن گوشت": "Ramen Goosht",
     "رامن مرغ": "Ramen Morgh",
-    "رامن مرغ، سیب زمینی و خامه": "Ramen Morgh (Potato & Cream)",
+    "رامن مرغ و خامه": "Ramen Morgh (+ Potato & Cream)",
     "رامن سس کیکو": "Ramen (Kiko Sauce)",
     "رامن سس بنجی": "Ramen (Benji Sauce)",
     "رامن سس وایتی": "Ramen (Waity Sauce)",
@@ -85,7 +85,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     "فوجی سالاد": "Fuji Salad",
     "وجی سالاد": "Veggie Salad",
     "آب": "Water",
-    "نوشابه": "Soda",
+    "اسپرایت": "Sprite",
+    "کوکا": "Coca",
+    "فانتا": "Fanta",
     "کیمچی": "Kimchi",
   };
 
